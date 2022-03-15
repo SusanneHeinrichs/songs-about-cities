70a92f5c94ddc7bcd5f1d4d103a33192d6eb252f
SusanneHeinrichs <susanneheinrichs1995@gmail.com>
2022-03-15 11:58:17 +0100

    Crawl through pagination

--- main.py
+++ main.py
@@ -2,26 +2,40 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.request import Request, urlopen
 import re
+import urllib.parse
 
 
-
-names_köln = ["Köln"]
-for name in names_köln:
-    #get all links from webpage
-    #tried using request with url copied from website (ö is causing problems)
+def request_soup(link):
     #downloads webpage data 
-    req = Request("https://www.songtexte.com/search?q=K%C3%B6ln&c=songs")
+    req = Request(link)
     html_page = urlopen(req)
     #loads it in a BeautifulSoup object
     soup = BeautifulSoup(html_page, "html.parser")
-    #gets all links and appends them to a list ('a' is indicator for links in html)
+    return soup
+
+names_köln = ["Köln"]
+for name in names_köln:
+    link = f"https://musikguru.de/search/?q={urllib.parse.quote(name)}&t=songs"
+    pages_total = request_soup(link).find("div", {"id": "PaginationInfo"}).get_text().split(" ")[-1]
     links = []
-    for link in soup.findAll('a'):
-        songtext_link = link.get('href')
-        #append only links that are leading to a songtext
-        if songtext_link.split("/")[0] == "songtext":
-            links.append(songtext_link)
-    print(links)
+    page = 1
+    
+    while page != int(pages_total) + 1:
+        if page == 1:
+            pass
+        else:
+            link = f"https://musikguru.de/search/?q={urllib.parse.quote(name)}&t=songs&p={page}"
+        soup = request_soup(link)
+        page += 1
+        #gets all links and appends them to a list ('a' is indicator for links in html)
+        for link in soup.findAll('a'):
+            songtext_link = link.get('href')
+            #append only links that are directing to a songtext
+            if "html" in songtext_link:
+                if songtext_link.split("/")[2].split("-")[0] == "songtext":
+                    links.append(songtext_link)
+        print(links)
+
     """
     # amount of list item childs inside the pagination list that we dont care about
     paginationListBullshitListElementsOffset = 2
